b2b922a45dfc3bfa4d384614aa2b34afe5f341f0
student <student@achiever-101.local>
2017-10-24 15:36:51 +0530

    Initial Commit

---
+++ brnPulseApplication/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  brnPulseApplication
+//
+//  Created by student on 10/24/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ brnPulseApplication/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  brnPulseApplication
+//
+//  Created by student on 10/24/17.
+//  Copyright © 2017 student. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
